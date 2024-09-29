c2fabb740fb08d756a25cdd1f7c2ef4e19394782
srijitt <srijit.chakraborty154@gmail.com>
2024-09-29 10:23:13 +0530

    removed lorem

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -72,7 +72,7 @@ function Login() {
         <div className='bg-layer w-[90vw] lg:w-[40vw] h-[60vh] lg:h-[50vh] xl:h-[60vh] py-10 rounded-lg flex flex-col items-center'>
           <div className='flex flex-col justify-center items-center'>
             <h1 className='text-secondary text-3xl font-pbold'>Login</h1>
-            <p className='mt-2 text-xs font-pregular w-2/3 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus, recusandae.</p>
+            <p className='mt-2 text-xs font-pregular w-2/3 text-center'>Access your account to explore more features and track your analysis history.</p>
           </div>
 
           <div className='text-sm text-red-400'>{error}</div>
